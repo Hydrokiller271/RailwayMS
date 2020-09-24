88fcf51066ce6604d6b878e115b0deb3569d6eeb
Hydrokiller271 <58259911+Hydrokiller271@users.noreply.github.com>
2020-09-24 08:58:59 +0700

    Update SQLQuery_Railway.sql

--- SQLQuery_Railway.sql
+++ SQLQuery_Railway.sql
@@ -2,30 +2,27 @@ CREATE DATABASE Railway
 GO
 USE Railway
 GO
-CREATE SCHEMA System
-GO
-
-CREATE TABLE System.Train(
+CREATE TABLE Train(
 	id VARCHAR (20) PRIMARY KEY,
 	name VARCHAR (20) );
 
-CREATE TABLE System.Station(
+CREATE TABLE Station(
 	id VARCHAR (20) PRIMARY KEY,
 	name VARCHAR (20) );
 
-CREATE TABLE System.Track(
+CREATE TABLE Track(
 	id VARCHAR (20) PRIMARY KEY,
 	station_a VARCHAR (20) ,
 	station_B VARCHAR (20) );
 
-CREATE TABLE System.Schedule(
+CREATE TABLE Schedule(
 	train VARCHAR (20) PRIMARY KEY ,
 	sequence_number VARCHAR (20) ,
 	station VARCHAR (20) ,
 	time_in TIME ,
 	time_out TIME);
 
-CREATE TABLE System.Booking(
+CREATE TABLE Booking(
 	id VARCHAR (20) PRIMARY KEY,
 	date Date ,
 	r_from VARCHAR (20) ,
